75ecb94a229e75c39098b0890b89a111fb4ea335
alfianadityads <alfianadityadwi@gmail.com>
2023-08-09 22:44:52 +0700

    push aja

--- pylondromme/src/Main.java
+++ pylondromme/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
-    private static boolean palindrome(String value)
+    private static boolean palindrome(String value) {
+
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
