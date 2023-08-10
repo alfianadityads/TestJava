c7bec1a7a00a841712caf4cb5af2313390e7acf2
alfianadityads <alfianadityadwi@gmail.com>
2023-08-11 00:06:47 +0700

    feat: make calculation volume tabung method in class tabung

--- volumebangunanOOP/src/Tabung.java
+++ volumebangunanOOP/src/Tabung.java
@@ -1,3 +1,14 @@
 public class Tabung {
+    int jariJari;
+    int tinggi;
 
+    public Tabung(int jariJari, int tinggi) {
+        this.jariJari = jariJari;
+        this.tinggi = tinggi;
+    }
+
+    public float hitungVolumeTabung() {
+        float luasLingkaran = ((22) * this.jariJari * this.jariJari) / 7;
+        return luasLingkaran * this.tinggi;
+    }
 }
