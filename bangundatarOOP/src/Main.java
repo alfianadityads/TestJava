412de7b10ca4f7cdf38ba000fa710d96cea5f69c
alfianadityads <alfianadityadwi@gmail.com>
2023-08-10 23:07:39 +0700

    feat: add hitung persegi panjang function class and method

--- bangundatarOOP/src/Main.java
+++ bangundatarOOP/src/Main.java
@@ -7,7 +7,6 @@ public class Main {
         System.out.println("Input sisi persegi : ");
         int sisiPersegi = input.nextInt();
 
-//        PERSEGI
         Persegi luasPersegi = new Persegi(sisiPersegi);
         luasPersegi.hitungLuasPersegi();
         System.out.println("Jadi, luas persegi tersebut adalah : " + luasPersegi.hitungLuasPersegi());
@@ -22,12 +21,23 @@ public class Main {
         System.out.println("Input tinggi segitiga : ");
         double tinggiSegitga = input.nextDouble();
 
-//        SEGITIGA
         Segitiga luasSegitiga = new Segitiga(alasSegitga,tinggiSegitga);
         luasSegitiga.hitungLuasSegitiga();
         System.out.println("Jadi, luas segitiga tersebut adalah : " + luasSegitiga.hitungLuasSegitiga());
         Segitiga kelilingSegitiga = new Segitiga(alasSegitga,tinggiSegitga);
         kelilingSegitiga.hitungKelilingSegitiga();
         System.out.println("Jadi, luas keliling tersebut adalah : " + kelilingSegitiga.hitungKelilingSegitiga());
+
+//        Persegi Panjang
+        System.out.println("Input panjang persegi panjang : ");
+        int panjangPersegiPanjang = input.nextInt();
+        System.out.println("Input lebar persegi panjang : ");
+        int lebarPersegiPanjang = input.nextInt();
+
+        PersegiPanjang luasPersegiPanjang = new PersegiPanjang(panjangPersegiPanjang,lebarPersegiPanjang);
+        luasPersegiPanjang.hitungLuasPersegiPanjang();
+        System.out.println("Jadi, luas persegi panjang tersebut adalah : " + luasPersegiPanjang.hitungLuasPersegiPanjang());
+        PersegiPanjang kelilingPersegiPanjang = new PersegiPanjang(panjangPersegiPanjang,lebarPersegiPanjang);
+        System.out.println("Jadi, keliling persegi panjang tersebut adalah : " + kelilingPersegiPanjang.hitungKelilingPersegiPanjang());
     }
 }
