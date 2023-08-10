ba72da6205a2b85e7319f291fc9452f02f2c88d2
alfianadityads <alfianadityadwi@gmail.com>
2023-08-10 22:42:07 +0700

    feat: add palindrome function

--- pylondromme/src/Main.java
+++ pylondromme/src/Main.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
 
     private static boolean palindrome(String value) {
+//        membuat variable penampung
+        char[] array = value.toCharArray();
+        char[] reverseArray = new char[array.length];
+        int temp = 0;
+        boolean res = false;
 
+//        looping huruf
+        for (int i = 0; i < value.length(); i++) {
+            reverseArray[i] = array[array.length - i - 1];
+        }
+
+//        memasukkan hasil looping ke dalam variable penampung
+        for (int i = 0; i < value.length(); i++) {
+            if (reverseArray[i] == array[i]) {
+                temp++;
+            }
+
+        }
+
+//        perkondisian apakah huruf sama atau tidak
+        if (temp == reverseArray.length) {
+            res = true;
+        } else {
+            res = false;
+        }
+
+        return res;
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Silahkan masukkan kata yang Anda akan cek : ");
+        Scanner input = new Scanner(System.in);
+        String value = input.nextLine();
+        System.out.println("Hsilnya : " + palindrome(value));
     }
 }
