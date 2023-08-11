e438b43aec4baab9196e99df3b154619a03c9b21
alfianadityads <alfianadityadwi@gmail.com>
2023-08-11 23:20:12 +0700

    feat: make input from user class parent

---
+++ OngkosKirim/src/Input.java
@@ -0,0 +1,21 @@
+public class Input {
+    float panjang, lebar, tinggi, berat;
+
+    public void InputDimensi(float panjang, float lebar, float tinggi) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+        this.tinggi = tinggi;
+    }
+
+    public void InputBerat(float berat) {
+        this.berat = berat;
+    }
+
+    public float hitungBerat() {
+        return 0;
+    }
+
+    public float hitungDimensi() {
+        return 0;
+    }
+}
