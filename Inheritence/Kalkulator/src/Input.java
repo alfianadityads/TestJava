eac0e2185ce0f2cd28e058a982398954ea4f6ff5
alfianadityads <alfianadityadwi@gmail.com>
2023-08-11 20:12:21 +0700

    chore: add input file, class and method

---
+++ Inheritence/Kalkulator/src/Input.java
@@ -0,0 +1,9 @@
+public class Input {
+//    membuat variable penampung inputan dari user
+    float input1, input2;
+
+    public Input(float input1, float input2) {
+        this.input1 = input1;
+        this.input2 = input2;
+    }
+}
